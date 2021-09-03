ae20e277cd24d62d514575682f6e652ec609cecf
angusho1 <angusho.77@gmail.com>
2021-09-02 22:36:43 -0700

    Got up and down traversal working with curses

--- src/displays/DirectoryPad.py
+++ src/displays/DirectoryPad.py
@@ -1,20 +1,22 @@
 import curses
 from typing import List
 from explorer.FileEntry import FileEntry, Directory
+from explorer.FileExplorer import FileExplorer
 
 
 class DirectoryPad:
-    def __init__(self, file_entries: List[FileEntry]) -> None:
+    def __init__(self, file_explorer: FileExplorer) -> None:
         self.DIR_COLOR = curses.color_pair(1)
         self.FILE_COLOR = curses.color_pair(2)
         self.SELECTED_COLOR = curses.color_pair(3)
 
-        self.file_entries = file_entries
+        self.file_explorer = file_explorer
+        self.file_entries = file_explorer.curr_dir_entries
         self.max_filename_len = self.get_max_filename_len()
         self.pad = self._create_pad()
-        self.refresh()
+        self.draw()
     
-    def refresh(self):
+    def draw(self):
         cursor_coords = curses.getsyx()
         for i, entry in enumerate(self.file_entries):
             if cursor_coords[0] == i:
@@ -23,29 +25,44 @@ class DirectoryPad:
                 self.pad.addstr(f'{entry.name}\n', self.DIR_COLOR)
             else:
                 self.pad.addstr(f'{entry.name}\n', self.FILE_COLOR)
+        self.noutrefresh()
+
+    def refresh(self):
         self.pad.refresh(0,0,  0,0, self.get_num_entries(), self.max_filename_len)
+
+    def noutrefresh(self):
+        self.pad.noutrefresh(0,0,  0,0, self.get_num_entries(), self.max_filename_len)
 
     def traverse_down(self):
         cursor_coords = curses.getsyx()
-        self.pad.chgat(self.DIR_COLOR)
-        self.pad.refresh(0,0,  0,0,  7, 16)
+        self.deselect_curr_file()
         self.pad.move(cursor_coords[0]+1, cursor_coords[1])
         self.pad.chgat(self.SELECTED_COLOR)
-        self.pad.refresh(0,0,  0,0,  7, 16)
+        self.noutrefresh()
 
     def traverse_up(self):
         cursor_coords = curses.getsyx()
-        self.pad.chgat(self.DIR_COLOR)
-        self.pad.refresh(0,0,  0,0,  7, 16)
+        self.deselect_curr_file()
         self.pad.move(cursor_coords[0]-1, cursor_coords[1])
         self.pad.chgat(self.SELECTED_COLOR)
-        self.pad.refresh(0,0,  0,0,  7, 16)
+        self.noutrefresh()
 
     def get_num_entries(self) -> int:
         return len(self.file_entries)
 
     def get_max_filename_len(self) -> int:
         return len(max(self.file_entries, key=lambda x: len(x.name)).name)
+
+    def deselect_curr_file(self):
+        curr_file = self.file_explorer.get_selected_entry()
+        if type(curr_file) == Directory:
+            self.pad.chgat(self.DIR_COLOR)
+        else:
+            self.pad.chgat(self.FILE_COLOR)
+
+    def go_to_start(self):
+        self.pad.move(0,0)
+        self.noutrefresh()
 
     def _create_pad(self):
         num_entries = self.get_num_entries()
--- src/main.py
+++ src/main.py
@@ -9,35 +9,36 @@ from displays.DirectoryPad import DirectoryPad
 def main(stdscr):
     stdscr.clear()
     curses.use_default_colors()
-    curses.init_pair(1, curses.COLOR_CYAN, -1)
+    curses.init_pair(1, curses.COLOR_MAGENTA, -1)
     curses.init_pair(2, curses.COLOR_GREEN, -1)
     curses.init_pair(3, curses.COLOR_YELLOW, -1)
     curses.curs_set(0)
-    stdscr.refresh()
+    stdscr.refresh()    # Need to call this before rendering anything
 
     DIR_COLOR = curses.color_pair(1)
     FILE_COLOR = curses.color_pair(2)
     SELECTED_COLOR = curses.color_pair(3)
 
     fe = FileExplorer()
-    curr_dir_pad = DirectoryPad(fe.curr_dir_entries)
+    curr_dir_pad = DirectoryPad(fe)
+    curr_dir_pad.go_to_start()
 
     # User interaction loop
     while True:
-        stdscr.refresh()
+        curses.doupdate()
         k = stdscr.getch()
         if k == curses.KEY_UP:
-            fe.traverse_up()
             curr_dir_pad.traverse_up()
+            fe.traverse_up()
         elif k == curses.KEY_DOWN:
-            fe.traverse_down()
             curr_dir_pad.traverse_down()
+            fe.traverse_down()
         elif k == curses.KEY_LEFT:
-            curr_dir_pad.addstr("KEYED LEFT\n", DIR_COLOR)
+            stdscr.addstr("KEYED LEFT\n", DIR_COLOR)
         elif k == curses.KEY_RIGHT:
-            curr_dir_pad.addstr("KEYED RIGHT\n", DIR_COLOR)
+            stdscr.addstr("KEYED RIGHT\n", DIR_COLOR)
         elif k == 10:
-            curr_dir_pad.addstr("KEYED ENTER\n", DIR_COLOR)
+            stdscr.addstr("KEYED ENTER\n", DIR_COLOR)
         if k == ord('q'):
             break
 
